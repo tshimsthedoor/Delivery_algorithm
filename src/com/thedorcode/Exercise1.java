33a1caeb63bcbb3d7c82a0c571f00524e3c319c6
tshimsthedoor <tshims79@gmail.com>
2023-10-12 22:58:19 +0200

    Delivery app

---
+++ src/com/thedorcode/Exercise1.java
@@ -0,0 +1,19 @@
+package com.thedorcode;
+
+public class Exercise1 {
+    public static void main(String[] args) {
+        int maxDistance = 10;
+        int distanceToHome = 1;
+
+        if(distanceToHome > maxDistance){
+            System.out.println("Distance from the store to your home is");
+            System.out.println(" more than " + maxDistance + "Km away.");
+            System.out.println("That is too far for free delivery.");
+        }
+        if(distanceToHome < maxDistance) {
+            System.out.println("Distance from the store to your home is");
+            System.out.println(" within " + maxDistance + "km away.");
+            System.out.println("You get free delivery!");
+        }
+    }
+}
---
+++ src/com/thedorcode/Main.java
@@ -0,0 +1,15 @@
+package com.thedorcode;
+
+public class Main {
+
+    public static void main(String[] args) {
+	    boolean happy = true;
+        boolean sad = false;
+
+        if(happy)
+            System.out.println("I am happy");
+
+        if(sad)
+            System.out.println("The variable sad is true");
+    }
+}
